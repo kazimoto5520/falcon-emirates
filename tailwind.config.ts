088806b970ab4c705187095e68fea41363e2ee56
kazimoto5520 <meshackkazimoto@gmail.com>
2024-04-27 22:52:22 +0300

    Font customization

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ['var(--font-mont)']
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
